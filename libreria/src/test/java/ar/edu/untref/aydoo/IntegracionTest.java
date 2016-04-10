1de6f3aac6ddf4fd3a97a0c8e2e74bcb574d91f3
Lucas <lucasdcampaner@gmail.com>
2016-04-09 22:58:17 -0300

    Configurar jdk 8.

--- libreria/src/test/java/ar/edu/untref/aydoo/IntegracionTest.java
+++ libreria/src/test/java/ar/edu/untref/aydoo/IntegracionTest.java
@@ -1,21 +1,14 @@
 package ar.edu.untref.aydoo;
 
-import org.junit.Assert;
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 public class IntegracionTest {
-	
+
 	@Test
-	public void calcularMontoACobrar() {
-		
-		
-		//A
-		
-		//A
-		
-		Assert.assertTrue(true);
-		
+	public void test() {
+		fail("Not yet implemented");
 	}
 
-	
 }
