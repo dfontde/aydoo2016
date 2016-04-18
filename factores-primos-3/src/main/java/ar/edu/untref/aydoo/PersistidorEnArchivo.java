26da5a5aa5e54953fc7aa0a70088c74bbd001868
lcampaner <lucas.campaner@grupoesfera.com.ar>
2016-04-18 17:50:37 -0300

    Disenio de la clase PersistidorEnArchivo.

--- factores-primos-3/src/main/java/ar/edu/untref/aydoo/PersistidorEnArchivo.java
+++ factores-primos-3/src/main/java/ar/edu/untref/aydoo/PersistidorEnArchivo.java
@@ -1,5 +1,37 @@
 package ar.edu.untref.aydoo;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class PersistidorEnArchivo {
 
+	private FileWriter archivoSalida;
+	private String path;
+	
+	public PersistidorEnArchivo(String path) throws IOException {
+		archivoSalida = new FileWriter(path);
+		this.path = path;
+	}
+	
+	public void escribirFactorizacion(String factorizacion) throws IOException {
+		archivoSalida.write(factorizacion);
+		archivoSalida.close();
+	}
+	
+	public String leerFactorizacion() throws FileNotFoundException {
+		
+		String factorizacion = "";
+		File archivoSalidaParaLeer = new File(this.path);
+		Scanner scanner = new Scanner(archivoSalidaParaLeer);
+		
+		while (scanner.hasNextLine()) {
+			factorizacion = scanner.nextLine();
+		}
+		
+		return factorizacion;
+	}
+	
 }
--- factores-primos-3/src/test/java/ar/edu/untref/aydoo/PersistidorEnArchivoTest.java
+++ factores-primos-3/src/test/java/ar/edu/untref/aydoo/PersistidorEnArchivoTest.java
@@ -1,15 +1,52 @@
 package ar.edu.untref.aydoo;
 
-import java.io.File;
+import java.io.IOException;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class PersistidorEnArchivoTest {
 
 	@Test
-	public void test() {
+	public void persistirEnArchivoEnDirectorioDelProyecto() throws IOException {
 		
-		File archivoSalida = new File("salida.txt");
+		String impresionEsperada = "Factores primos 90: 2 2 2 3 3 5 ";
+		String path = "salida.txt";
+		PersistidorEnArchivo persistidorEnArchivo = new PersistidorEnArchivo(path);
+		persistidorEnArchivo.escribirFactorizacion(impresionEsperada);
+		
+		String impresionObtenida = persistidorEnArchivo.leerFactorizacion();
+		
+		Assert.assertEquals(impresionEsperada, impresionObtenida);
+	
+	}
+
+//	@Test
+//	public void persistirEnArchivoEnVariasLineasEnDirectorioDelProyecto() throws IOException {
+//		
+//		String impresionEsperada = "2\n2\n2\n3\n3\n5\n";
+//		String path = "salida.txt";
+//		PersistidorEnArchivo persistidorEnArchivo = new PersistidorEnArchivo(path);
+//		persistidorEnArchivo.escribirFactorizacion(impresionEsperada);
+//		
+//		String impresionObtenida = persistidorEnArchivo.leerFactorizacion();
+//		
+//		Assert.assertEquals(impresionEsperada, impresionObtenida);
+//	
+//	}
+	
+	@Test
+	public void persistirEnArchivoEnUnPathDefinido() throws IOException {
+		
+		String impresionEsperada = "Factores primos 90: 2 2 2 3 3 5 ";
+		String path = "/home/lucas/aydoo2016/factores-primos-3/archivosSalida/salida.txt";
+		
+		PersistidorEnArchivo persistidorEnArchivo = new PersistidorEnArchivo(path);
+		persistidorEnArchivo.escribirFactorizacion(impresionEsperada);
+		
+		String impresionObtenida = persistidorEnArchivo.leerFactorizacion();
+		
+		Assert.assertEquals(impresionEsperada, impresionObtenida);
 	
 	}
 
