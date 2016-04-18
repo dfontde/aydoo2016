24cf45e6df7a81119d5a5bf647eb8968c9bf6644
Lucas <lucasdcampaner@gmail.com>
2016-04-18 00:08:36 -0300

    Disenio de la clase que va a persistir la salida.

---
+++ factores-primos-3/src/test/java/ar/edu/untref/aydoo/PersistidorEnArchivoTest.java
@@ -0,0 +1,16 @@
+package ar.edu.untref.aydoo;
+
+import java.io.File;
+
+import org.junit.Test;
+
+public class PersistidorEnArchivoTest {
+
+	@Test
+	public void test() {
+		
+		File archivoSalida = new File("salida.txt");
+	
+	}
+
+}
