b614f39c24ea460468dcbf59dc93aa56a5b77ff3
lcampaner <lucas.campaner@grupoesfera.com.ar>
2016-04-19 17:09:59 -0300

    Creacion de clase para obtener las funcionalidades.

---
+++ factores-primos-3/src/test/java/ar/edu/untref/aydoo/AdministradorDeFuncionalidadesTest.java
@@ -0,0 +1,85 @@
+package ar.edu.untref.aydoo;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AdministradorDeFuncionalidadesTest {
+
+	@Test
+	public void obtenerFuncionalidadesDesdeArrayDeArgumentos() {
+		
+		String formatoEsperado = "--FORMAT=PRETTY";
+		String sortEsperado = "--SORT:ASC";
+		String outputEsperado = "--OUTPUT-FILE:ARCHIVOSALIDA.TXT";
+		
+		String[] args = {"360", formatoEsperado, sortEsperado, outputEsperado};
+		AdministradorDeFuncionalidades administradorDeFuncionalidades = new AdministradorDeFuncionalidades(args);
+		
+		String formatObtenido = administradorDeFuncionalidades.getFormat();
+		String sortObtenido = administradorDeFuncionalidades.getSort();
+		String outputObtenido = administradorDeFuncionalidades.getOutput();
+		
+		Assert.assertEquals(formatoEsperado, formatObtenido);
+		Assert.assertEquals(sortEsperado, sortObtenido);
+		Assert.assertEquals(outputEsperado, outputObtenido);
+		
+	}
+
+	@Test
+	public void obtenerFuncionalidadesDesdeArrayDeArgumentosEnDistintoOrden1() {
+		
+		String formatoEsperado = "--FORMAT=PRETTY";
+		String sortEsperado = "--SORT:ASC";
+		String outputEsperado = "--OUTPUT-FILE:ARCHIVOSALIDA.TXT";
+		
+		String[] args = {"360", sortEsperado, outputEsperado, formatoEsperado};
+		AdministradorDeFuncionalidades administradorDeFuncionalidades = new AdministradorDeFuncionalidades(args);
+		
+		String formatObtenido = administradorDeFuncionalidades.getFormat();
+		String sortObtenido = administradorDeFuncionalidades.getSort();
+		String outputObtenido = administradorDeFuncionalidades.getOutput();
+		
+		Assert.assertEquals(formatoEsperado, formatObtenido);
+		Assert.assertEquals(sortEsperado, sortObtenido);
+		Assert.assertEquals(outputEsperado, outputObtenido);
+		
+	}
+
+	@Test
+	public void obtenerFuncionalidadesDesdeArrayDeArgumentosEnDistintoOrden2() {
+		
+		String formatoEsperado = "--FORMAT=PRETTY";
+		String sortEsperado = "--SORT:ASC";
+		String outputEsperado = "--OUTPUT-FILE:ARCHIVOSALIDA.TXT";
+		
+		String[] args = {"360", outputEsperado, sortEsperado, formatoEsperado};
+		AdministradorDeFuncionalidades administradorDeFuncionalidades = new AdministradorDeFuncionalidades(args);
+		
+		String formatObtenido = administradorDeFuncionalidades.getFormat();
+		String sortObtenido = administradorDeFuncionalidades.getSort();
+		String outputObtenido = administradorDeFuncionalidades.getOutput();
+		
+		Assert.assertEquals(formatoEsperado, formatObtenido);
+		Assert.assertEquals(sortEsperado, sortObtenido);
+		Assert.assertEquals(outputEsperado, outputObtenido);
+		
+	}
+
+	@Test
+	public void obtenerFuncionalidadesDesdeArrayDeArgumentosSinOutput() {
+		
+		String formatoEsperado = "--FORMAT=PRETTY";
+		String sortEsperado = "--SORT:ASC";
+		
+		String[] args = {"360", sortEsperado, formatoEsperado};
+		AdministradorDeFuncionalidades administradorDeFuncionalidades = new AdministradorDeFuncionalidades(args);
+		
+		String formatObtenido = administradorDeFuncionalidades.getFormat();
+		String sortObtenido = administradorDeFuncionalidades.getSort();
+		
+		Assert.assertEquals(formatoEsperado, formatObtenido);
+		Assert.assertEquals(sortEsperado, sortObtenido);
+		
+	}
+
+}
