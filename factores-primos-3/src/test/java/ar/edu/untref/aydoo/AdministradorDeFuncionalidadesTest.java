9832b2774a111da3166ec614d02205e3ca8ac6f9
Lucas <lucasdcampaner@gmail.com>
2016-04-19 21:28:02 -0300

    Test que cubren la falta de parametros  para el Administrador de funcionalidades.

--- factores-primos-3/src/test/java/ar/edu/untref/aydoo/AdministradorDeFuncionalidadesTest.java
+++ factores-primos-3/src/test/java/ar/edu/untref/aydoo/AdministradorDeFuncionalidadesTest.java
@@ -85,4 +85,44 @@ public class AdministradorDeFuncionalidadesTest {
 		
 	}
 
+	@Test
+	public void obtenerFuncionalidadesDesdeArrayDeArgumentosSinSort() {
+		
+		String formatoEsperado = "--FORMAT=PRETTY";
+		String sortEsperado = "";
+		String outputEsperado = "--OUTPUT-FILE:ARCHIVOSALIDA.TXT";
+		
+		String[] args = {"360", outputEsperado, formatoEsperado};
+		AdministradorDeFuncionalidades administradorDeFuncionalidades = new AdministradorDeFuncionalidades(args);
+		
+		String formatObtenido = administradorDeFuncionalidades.getFormat();
+		String sortObtenido = administradorDeFuncionalidades.getSort();
+		String outputObtenido = administradorDeFuncionalidades.getOutput();
+		
+		Assert.assertEquals(formatoEsperado, formatObtenido);
+		Assert.assertEquals(sortEsperado, sortObtenido);
+		Assert.assertEquals(outputEsperado, outputObtenido);
+		
+	}
+
+	@Test
+	public void obtenerFuncionalidadesDesdeArrayDeArgumentosSinFormat() {
+		
+		String formatoEsperado = "";
+		String sortEsperado = "--SORT:ASC";
+		String outputEsperado = "--OUTPUT-FILE:ARCHIVOSALIDA.TXT";
+		
+		String[] args = {"360", outputEsperado, sortEsperado};
+		AdministradorDeFuncionalidades administradorDeFuncionalidades = new AdministradorDeFuncionalidades(args);
+		
+		String formatObtenido = administradorDeFuncionalidades.getFormat();
+		String sortObtenido = administradorDeFuncionalidades.getSort();
+		String outputObtenido = administradorDeFuncionalidades.getOutput();
+		
+		Assert.assertEquals(formatoEsperado, formatObtenido);
+		Assert.assertEquals(sortEsperado, sortObtenido);
+		Assert.assertEquals(outputEsperado, outputObtenido);
+		
+	}
+
 }
