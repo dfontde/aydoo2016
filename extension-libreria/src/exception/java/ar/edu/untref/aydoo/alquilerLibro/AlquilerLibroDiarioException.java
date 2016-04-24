b3a82466f2f660e8bd6979383ff6f4344b65bbe0
Lucas <lucasdcampaner@gmail.com>
2016-04-24 13:33:46 -0300

    Creacion del source folder para las excepciones.

---
+++ extension-libreria/src/exception/java/ar/edu/untref/aydoo/alquilerLibro/AlquilerLibroDiarioException.java
@@ -0,0 +1,12 @@
+package ar.edu.untref.aydoo.alquilerLibro;
+
+public class AlquilerLibroDiarioException extends AlquilerLibroExcepcion {
+
+	private static final long serialVersionUID = 1L;
+	private final static String mensaje = "El periodo de alquiler es entre 3 y 25 dias.";
+
+	public AlquilerLibroDiarioException() {
+		super(mensaje);
+	}
+
+}
---
+++ extension-libreria/src/exception/java/ar/edu/untref/aydoo/alquilerLibro/AlquilerLibroExcepcion.java
@@ -0,0 +1,11 @@
+package ar.edu.untref.aydoo.alquilerLibro;
+
+public class AlquilerLibroExcepcion extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public AlquilerLibroExcepcion(String mensaje) {
+		super(mensaje);
+	}
+
+}
