f28be6b37582fb086e85174465171d96e2307a90
Lucas <lucasdcampaner@gmail.com>
2016-04-25 20:57:33 -0300

    Refactor AlquilerLibro.

--- extension-libreria/src/main/java/ar/edu/untref/aydoo/AlquilerLibro.java
+++ extension-libreria/src/main/java/ar/edu/untref/aydoo/AlquilerLibro.java
@@ -3,12 +3,12 @@ package ar.edu.untref.aydoo;
 import ar.edu.untref.aydoo.exception.AlquilerLibroExcepcion;
 
 public abstract class AlquilerLibro extends Producto {
-	
+
 	public AlquilerLibro() {
 		super(0, false);
 	}
 
 	@Override
-	public abstract double obtenerPrecioDelProducto () throws AlquilerLibroExcepcion;
-		
+	public abstract double obtenerPrecioDelProducto() throws AlquilerLibroExcepcion;
+
 }
