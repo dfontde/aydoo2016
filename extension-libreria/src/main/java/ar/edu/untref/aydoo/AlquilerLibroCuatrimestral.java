f8fcba6cf6e7c381fe239d6b6dc40e7518ae6848
Lucas <lucasdcampaner@gmail.com>
2016-04-25 20:58:07 -0300

    Refactor AlquilerLibroCuatrimestral.

--- extension-libreria/src/main/java/ar/edu/untref/aydoo/AlquilerLibroCuatrimestral.java
+++ extension-libreria/src/main/java/ar/edu/untref/aydoo/AlquilerLibroCuatrimestral.java
@@ -7,26 +7,27 @@ public class AlquilerLibroCuatrimestral extends AlquilerLibro {
 	private final double porcentajeDescuento = 10.0;
 	private final double cien = 100.0;
 	private final double precioAlquilerPorCuatrimestre = 800.0;
-	
+
 	private int tiempoEnAlquiler;
-	
+
 	public AlquilerLibroCuatrimestral(int tiempoEnAlquiler) {
 		this.tiempoEnAlquiler = tiempoEnAlquiler;
 	}
 
 	@Override
 	public double obtenerPrecioDelProducto() throws AlquilerLibroCuatrimestralException {
-		
+
 		double precioAlquiler = 0.0;
-		
-		if (tiempoEnAlquiler < 1 || tiempoEnAlquiler >  2) {
+
+		if (tiempoEnAlquiler < 1 || tiempoEnAlquiler > 2) {
 			throw new AlquilerLibroCuatrimestralException();
 		}
-		
-		precioAlquiler = (precioAlquilerPorCuatrimestre - (precioAlquilerPorCuatrimestre * porcentajeDescuento /cien)) * tiempoEnAlquiler;
-		
+
+		precioAlquiler = (precioAlquilerPorCuatrimestre - (precioAlquilerPorCuatrimestre * porcentajeDescuento / cien))
+				* tiempoEnAlquiler;
+
 		return precioAlquiler;
 
 	}
-	
+
 }
