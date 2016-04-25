6b9308def5de40d63a19a72a463bf2b16a811a2c
lucas <lucasdcampaner@gmail.com>
2016-04-25 20:09:14 -0300

    Excepcion para alquileres cuatrimestrales.

--- extension-libreria/src/main/java/ar/edu/untref/aydoo/AlquilerLibroCuatrimestral.java
+++ extension-libreria/src/main/java/ar/edu/untref/aydoo/AlquilerLibroCuatrimestral.java
@@ -1,5 +1,7 @@
 package ar.edu.untref.aydoo;
 
+import ar.edu.untref.aydoo.exception.AlquilerLibroCuatrimestralException;
+
 public class AlquilerLibroCuatrimestral extends AlquilerLibro {
 
 	final double porcentajeDescuento = 10.0;
@@ -13,13 +15,15 @@ public class AlquilerLibroCuatrimestral extends AlquilerLibro {
 	}
 
 	@Override
-	public double obtenerPrecioDelProducto (){
+	public double obtenerPrecioDelProducto() throws AlquilerLibroCuatrimestralException {
 		
 		double precioAlquiler = 0.0;
 		
-		if (tiempoEnAlquiler > 0 && tiempoEnAlquiler < 3) {
-			precioAlquiler = (precioAlquilerPorCuatrimestre - (precioAlquilerPorCuatrimestre * porcentajeDescuento /cien)) * tiempoEnAlquiler;
+		if (tiempoEnAlquiler < 0 || tiempoEnAlquiler >  2) {
+			throw new AlquilerLibroCuatrimestralException();
 		}
+		
+		precioAlquiler = (precioAlquilerPorCuatrimestre - (precioAlquilerPorCuatrimestre * porcentajeDescuento /cien)) * tiempoEnAlquiler;
 		
 		return precioAlquiler;
 
---
+++ extension-libreria/src/main/java/ar/edu/untref/aydoo/exception/AlquilerLibroCuatrimestralException.java
@@ -0,0 +1,12 @@
+package ar.edu.untref.aydoo.exception;
+
+public class AlquilerLibroCuatrimestralException extends AlquilerLibroExcepcion {
+	
+	private static final long serialVersionUID = 1L;
+	private final static String mensaje = "El periodo de alquiler es entre 1 y 2 cuatrimestres.";
+
+	public AlquilerLibroCuatrimestralException() {
+		super(mensaje);
+	}
+
+}
