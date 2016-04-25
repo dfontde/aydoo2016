dd2951317c10080e0ff6d854834ef3ac04417912
Lucas <lucasdcampaner@gmail.com>
2016-04-25 20:58:47 -0300

    Refactor AlquilerLibroMensual.

--- extension-libreria/src/main/java/ar/edu/untref/aydoo/AlquilerLibroMensual.java
+++ extension-libreria/src/main/java/ar/edu/untref/aydoo/AlquilerLibroMensual.java
@@ -5,24 +5,24 @@ import ar.edu.untref.aydoo.exception.AlquilerLibroMensualException;
 public class AlquilerLibroMensual extends AlquilerLibro {
 
 	private final double precioAlquilerPorMes = 200.0;
-	
+
 	private int tiempoEnAlquiler;
-	
+
 	public AlquilerLibroMensual(int tiempoEnAlquiler) {
 		this.tiempoEnAlquiler = tiempoEnAlquiler;
 	}
 
 	@Override
-	public double obtenerPrecioDelProducto () throws AlquilerLibroMensualException{
-		
+	public double obtenerPrecioDelProducto() throws AlquilerLibroMensualException {
+
 		double precioAlquiler = 0.0;
-	
+
 		if (tiempoEnAlquiler < 1 || tiempoEnAlquiler > 3) {
 			throw new AlquilerLibroMensualException();
-		}		
-		
-		precioAlquiler = tiempoEnAlquiler * precioAlquilerPorMes;	
-		
+		}
+
+		precioAlquiler = tiempoEnAlquiler * precioAlquilerPorMes;
+
 		return precioAlquiler;
 
 	}
