6c0c958f8fcbec5a29e081d120afcf6507cf0969
Lucas <lucasdcampaner@gmail.com>
2016-04-25 20:55:28 -0300

    Test de borde para alquiler mensual.

--- extension-libreria/src/test/java/ar/edu/untref/aydoo/AlquilerLibroTest.java
+++ extension-libreria/src/test/java/ar/edu/untref/aydoo/AlquilerLibroTest.java
@@ -12,29 +12,29 @@ public class AlquilerLibroTest {
 
 	@Test
 	public void alquilarLibroPor7DiasCuesta70Pesos() throws AlquilerLibroExcepcion {
-		
+
 		int tiempoEnAlquiler = 7;
 		Producto libroCrimenYCastigo = new AlquilerLibroDiario(tiempoEnAlquiler);
 		double precioAlquilerEsperado = 70.0;
-		
+
 		double precioAlquilerObtenido = libroCrimenYCastigo.obtenerPrecioDelProducto();
-			
-		Assert.assertEquals(precioAlquilerEsperado, precioAlquilerObtenido,0);
+
+		Assert.assertEquals(precioAlquilerEsperado, precioAlquilerObtenido, 0);
 	}
 
-	@Test(expected=AlquilerLibroDiarioException.class)
-	public void alquilarLibroPor2DiasLanzaExcepcion() throws AlquilerLibroExcepcion{
-		
+	@Test(expected = AlquilerLibroDiarioException.class)
+	public void alquilarLibroPor2DiasLanzaExcepcion() throws AlquilerLibroExcepcion {
+
 		int tiempoEnAlquiler = 2;
 		Producto libroCrimenYCastigo = new AlquilerLibroDiario(tiempoEnAlquiler);
-	
+
 		libroCrimenYCastigo.obtenerPrecioDelProducto();
-	
+
 	}
 
-	@Test(expected=AlquilerLibroDiarioException.class)
+	@Test(expected = AlquilerLibroDiarioException.class)
 	public void alquilarLibroPor26DiasLanzaExcepcion() throws AlquilerLibroExcepcion {
-		
+
 		int tiempoEnAlquiler = 26;
 		Producto libroCrimenYCastigo = new AlquilerLibroDiario(tiempoEnAlquiler);
 
@@ -44,84 +44,90 @@ public class AlquilerLibroTest {
 
 	@Test
 	public void alquilarLibroPor2MesesCuesta400Pesos() throws AlquilerLibroExcepcion {
-		
+
 		int tiempoEnAlquiler = 2;
 		Producto libroCrimenYCastigo = new AlquilerLibroMensual(tiempoEnAlquiler);
 		double precioAlquilerEsperado = 400.0;
-		
+
 		double precioAlquilerObtenido = libroCrimenYCastigo.obtenerPrecioDelProducto();
-			
-		Assert.assertEquals(precioAlquilerEsperado, precioAlquilerObtenido,0);
+
+		Assert.assertEquals(precioAlquilerEsperado, precioAlquilerObtenido, 0);
 	}
 
 	@Test
 	public void alquilarLibroPor3MesesCuesta600Pesos() throws AlquilerLibroExcepcion {
-		
+
 		int tiempoEnAlquiler = 3;
 		Producto libroCrimenYCastigo = new AlquilerLibroMensual(tiempoEnAlquiler);
 		double precioAlquilerEsperado = 600.0;
-		
+
 		double precioAlquilerObtenido = libroCrimenYCastigo.obtenerPrecioDelProducto();
-			
-		Assert.assertEquals(precioAlquilerEsperado, precioAlquilerObtenido,0);
+
+		Assert.assertEquals(precioAlquilerEsperado, precioAlquilerObtenido, 0);
 	}
 
-	@Test(expected=AlquilerLibroMensualException.class)
+	@Test(expected = AlquilerLibroMensualException.class)
 	public void alquilarLibroPor4MesesLanzaExcepcion() throws AlquilerLibroExcepcion {
-		
+
 		int tiempoEnAlquiler = 4;
 		Producto libroCrimenYCastigo = new AlquilerLibroMensual(tiempoEnAlquiler);
-		
+
 		libroCrimenYCastigo.obtenerPrecioDelProducto();
-			
+
+	}
+
+	@Test(expected = AlquilerLibroMensualException.class)
+	public void alquilarLibroPor0MesesLanzaExcepcion() throws AlquilerLibroExcepcion {
+
+		int tiempoEnAlquiler = 0;
+		Producto libroCrimenYCastigo = new AlquilerLibroMensual(tiempoEnAlquiler);
+
+		libroCrimenYCastigo.obtenerPrecioDelProducto();
+
 	}
 
 	@Test
 	public void alquilarLibroPor1CuatrimestreCuesta720Pesos() throws AlquilerLibroExcepcion {
-		
+
 		int tiempoEnAlquiler = 1;
 		Producto libroCrimenYCastigo = new AlquilerLibroCuatrimestral(tiempoEnAlquiler);
 		double precioAlquilerEsperado = 720.0;
-		
+
 		double precioAlquilerObtenido = libroCrimenYCastigo.obtenerPrecioDelProducto();
-			
-		Assert.assertEquals(precioAlquilerEsperado, precioAlquilerObtenido,0);
+
+		Assert.assertEquals(precioAlquilerEsperado, precioAlquilerObtenido, 0);
 	}
 
 	@Test
 	public void alquilarLibroPor2CuatrimestreCuesta1440Pesos() throws AlquilerLibroExcepcion {
-		
+
 		int tiempoEnAlquiler = 2;
 		Producto libroCrimenYCastigo = new AlquilerLibroCuatrimestral(tiempoEnAlquiler);
 		double precioAlquilerEsperado = 1440.0;
-		
+
 		double precioAlquilerObtenido = libroCrimenYCastigo.obtenerPrecioDelProducto();
-			
-		Assert.assertEquals(precioAlquilerEsperado, precioAlquilerObtenido,0);
+
+		Assert.assertEquals(precioAlquilerEsperado, precioAlquilerObtenido, 0);
 	}
 
-	@Test(expected=AlquilerLibroCuatrimestralException.class)
+	@Test(expected = AlquilerLibroCuatrimestralException.class)
 	public void alquilarLibroPor3CuatrimestreLanzaExcepcion() throws AlquilerLibroExcepcion {
-		
+
 		int tiempoEnAlquiler = 3;
 		Producto libroCrimenYCastigo = new AlquilerLibroCuatrimestral(tiempoEnAlquiler);
-		
+
 		libroCrimenYCastigo.obtenerPrecioDelProducto();
-			
+
 	}
 
-	@Test(expected=AlquilerLibroCuatrimestralException.class)
+	@Test(expected = AlquilerLibroCuatrimestralException.class)
 	public void alquilarLibroPor0CuatrimestreLanzaExcepcion() throws AlquilerLibroExcepcion {
-		
+
 		int tiempoEnAlquiler = 0;
 		Producto libroCrimenYCastigo = new AlquilerLibroCuatrimestral(tiempoEnAlquiler);
-		
+
 		libroCrimenYCastigo.obtenerPrecioDelProducto();
-			
+
 	}
 
 }
-
-
-
-
