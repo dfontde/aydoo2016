3e6d632d3f8b3e62fc44992cbb7d18fb4156b416
Lucas <lucasdcampaner@gmail.com>
2016-04-25 21:04:23 -0300

    Refactor AlquilerLibroCuatrimestralException.

--- extension-libreria/src/main/java/ar/edu/untref/aydoo/exception/AlquilerLibroCuatrimestralException.java
+++ extension-libreria/src/main/java/ar/edu/untref/aydoo/exception/AlquilerLibroCuatrimestralException.java
@@ -1,7 +1,7 @@
 package ar.edu.untref.aydoo.exception;
 
 public class AlquilerLibroCuatrimestralException extends AlquilerLibroException {
-	
+
 	private static final long serialVersionUID = 1L;
 	private final static String mensaje = "El periodo de alquiler es entre 1 y 2 cuatrimestres.";
 
